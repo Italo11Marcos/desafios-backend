3f065d9e3e193e76ea607c74d7a78897d8e3fa13
Ítalo Marcos <italomarcosboa@gmail.com>
2021-08-18 16:54:36 -0300

    API Resource Categoria

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\categoria;
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
+    public function __construct(Categoria $categoria){
+        $this->categoria = $categoria;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +17,8 @@ class CategoriaController extends Controller
      */
     public function index()
     {
-        //
+        $categorias = $this->categoria->all();
+        return response()->json($categorias, 200);
     }
 
     /**
@@ -25,40 +29,69 @@ class CategoriaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->categoria->rules(), $this->categoria->message());
+        $categoria = $this->categoria->create($request->all());
+        return response()->json($categoria, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\categoria  $categoria
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function show(categoria $categoria)
+    public function show($id)
     {
-        //
+        $categoria = $this->categoria->find($id);
+        if($categoria === null){
+            return response()->json(['erro' => 'Categoria not found'], 404);
+        }
+        return response()->json($categoria, 200);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\categoria  $categoria
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, categoria $categoria)
+    public function update(Request $request, $id)
     {
-        //
+        $categoria = $this->categoria->find($id);
+        
+        if($categoria === null){
+            return response()->json(['erro' => 'Categoria not found'], 404);
+        }
+
+        if($request->method() === 'PATCH') {
+            $dinamicRules = array();
+            foreach($categoria->rules() as $input => $regra){
+                if(array_key_exists($input, $request->all())){
+                    $dinamicRules[$input] = $regra;
+                }
+            }
+        }else {
+            $request->validate($this->categoria->rules(), $this->categoria->message());
+        }
+
+        $categoria->update($request->all());
+        return response()->json($categoria, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\categoria  $categoria
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(categoria $categoria)
+    public function destroy($id)
     {
-        //
+        $categoria = $this->categoria->find($id);
+        if($categoria === null){
+            return response()->json(['erro' => 'Categoria not found'], 404);
+        }
+        $categoria->delete();
+        return response()->json(['msg' => 'Delete success'], 200);
     }
 }
--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Categoria extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nome'];
+
+    public function rules(){
+        return [
+            'nome' => 'required|unique:categorias,nome,'.$this->id.'|max:30'
+        ];
+    }
+
+    public function message(){
+        return [
+            'required' => 'Field :attribute is required',
+            'unique' => 'Nome already exists',
+            'max' => 'Max 30 caracteres'
+        ];
+    }
 }
