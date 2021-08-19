b6b97a95139cdcb1150b8479934b77bbefab696d
Ítalo Marcos <italomarcosboa@gmail.com>
2021-08-19 18:21:08 -0300

    Add JWT-auth

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::prefix('v1')->middleware('jwt.auth')->group(function () {
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::apiResource('categoria', 'App\Http\Controllers\CategoriaController');
+    Route::apiResource('composicao', 'App\Http\Controllers\ComposicaoController');
+    Route::apiResource('produto', 'App\Http\Controllers\ProdutoController');
 });
 
-Route::apiResource('categoria', 'App\Http\Controllers\CategoriaController');
-Route::apiResource('composicao', 'App\Http\Controllers\ComposicaoController');
-Route::apiResource('produto', 'App\Http\Controllers\ProdutoController');
+Route::post('login', 'App\Http\Controllers\AuthController@login');
