57c8dcecc7867b588e3ca4f5dd99246a6989d4b3
italo11marcos <italomarcosboa@gmail.com>
2021-09-29 18:31:22 -0300

    NEW - Criando a url e view index

--- covid/views.py
+++ covid/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    days = 0
+    if request.method == 'POST':
+        days = request.POST.get('days', False)
+    context = {'days': days}
+    return render(request, 'index.html', context)
