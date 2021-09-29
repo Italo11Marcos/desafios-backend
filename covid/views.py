6f3f7ccb88b0956e77354404414313892989f254
italo11marcos <italomarcosboa@gmail.com>
2021-09-29 19:53:18 -0300

    NEW - Implementação da função predict

--- covid/views.py
+++ covid/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .predict import predict
 
 # Create your views here.
 def index(request):
-    days = 0
+    news_cases = 0
     if request.method == 'POST':
         days = request.POST.get('days', False)
-    context = {'days': days}
+        news_cases = predict(days)
+    context = {'news_cases': news_cases}
     return render(request, 'index.html', context)
