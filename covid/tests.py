c4684c33c279e1adaea008e7bdf73c5428779642
italo11marcos <italomarcosboa@gmail.com>
2021-09-29 21:32:27 -0300

    NEW - Aplicação de testes

--- covid/tests.py
+++ covid/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+class IndexViewTestCase(TestCase):
+
+    def setUp(self):
+        self.dados = {
+            'days': 5
+        }
+        self.client = Client()
+
+    #verifica se o response da 200
+    def test_index_post(self):
+        response = self.client.post('', self.dados)
+
+        self.assertEqual(response.status_code, 200)
+
+    #verifica se o response context retorna a quantidade certa
+    def test_return_post(self):
+        response = self.client.post('', self.dados)
+
+        self.assertEqual(len(response.context['news_cases']), 5)
