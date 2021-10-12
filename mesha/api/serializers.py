529f7ed70ad7fdde7d222626862e5a9cd8b90bbf
italo11marcos <italomarcosboa@gmail.com>
2021-10-12 15:09:09 -0300

    feat: adicionado metodo update no LivroSerializer

--- mesha/api/serializers.py
+++ mesha/api/serializers.py
@@ -6,7 +6,7 @@ class AutorSerializer(ModelSerializer):
 
     class Meta:
         model = Autor
-        fields = ('id', 'nome')
+        fields = ['nome']
 
 
 class LivroSerializer(ModelSerializer):
@@ -15,17 +15,33 @@ class LivroSerializer(ModelSerializer):
 
     class Meta:
         model = Livro
-        fields = ('id', 'titulo', 'editora', 'autores')
+        fields = ('id', 'titulo', 'editora', 'foto', 'autores')
 
     def create_autores(self, autores, livro):
         for autor in autores:
             obj = Autor.objects.create(**autor)
             livro.autores.add(obj)
 
-    def create(self, validated_data, **kwargs):
+    def create(self, validated_data):
         autores = validated_data.pop('autores')
 
         livro = Livro.objects.create(**validated_data)
         self.create_autores(autores, livro)
 
         return livro
+
+    def update(self, instance, validated_data):
+        autores_data = validated_data.pop('autores')
+        instance = super(LivroSerializer, self).update(instance, validated_data)
+
+        for autor_data in autores_data:
+            autor_qs = Autor.objects.filter(nome__iexact=autor_data['nome'])
+
+            if autor_qs.exists():
+                autor = autor_qs.first()
+            else:
+                autor = Autor.objects.create(**autor_data)
+
+            instance.autores.add(autor)
+
+        return instance
