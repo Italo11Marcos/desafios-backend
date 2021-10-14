2c22a5d95d965001991749fcc3b708806999ef1a
Ítalo Marcos <italomarcosboa@gmail.com>
2021-10-14 19:53:47 -0300

    iniciando o projeto

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require_once("templates/painel/header.php");
+require_once("globals.php");
+require_once("db.php");
+?>
+
+<section class="content">
+<div class="content-wrapper">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Admin</h3>
+        </div>
+        <div class="card-body">
+            Ola Mundo
+        </div>
+    </div>
+</div>
+</section>
+
+<?php
+require_once("templates/painel/footer.php");
+?>
