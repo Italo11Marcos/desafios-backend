86988a570925cda3f9ef9f55ebb5c177203c6527
Ítalo Marcos <italomarcosboa@gmail.com>
2021-10-15 17:10:17 -0300

    feat: adicao session messages

---
+++ session_messages.php
@@ -0,0 +1,22 @@
+<?php
+    if(!empty($_SESSION['campos'])){
+        echo $_SESSION['campos'];
+        unset($_SESSION['campos']);
+    }
+    if(!empty($_SESSION['login'])){
+        echo $_SESSION['login'];
+        unset($_SESSION['login']);
+    }
+    if(!empty($_SESSION['password'])){
+        echo $_SESSION['password'];
+        unset($_SESSION['password']);
+    }
+    if(!empty($_SESSION['sucesso'])){
+        echo $_SESSION['sucesso'];
+        unset($_SESSION['sucesso']);
+    }
+    if(!empty($_SESSION['erro'])){
+        echo $_SESSION['erro'];
+        unset($_SESSION['erro']);
+    }
+?>
