6856eade0145ca39ca9f4cd05310b334423aa03b
Ítalo Marcos <italomarcosboa@gmail.com>
2021-10-17 09:16:47 -0300

    refactor: headers login

--- templates/login/header.php
+++ templates/login/header.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Log in</title>
+  <link rel="shortcut icon" href="https://nanoincub.com.br/wp-content/uploads/2021/05/nano.png" type="image/x-icon">
+  <title>Nano</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
